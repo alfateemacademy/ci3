c9e0e5ac8ba268d5df4567af919a659d4e226025
owais <owaisrafiq9@gmail.com>
2015-10-05 19:53:13 -0700

    New controller file added

---
+++ application/controllers/country.php
@@ -0,0 +1,15 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Country extends CI_Controller {
+
+	public function index()
+	{
+		$this->load->view('welcome_message');
+	}
+
+	public function about()
+	{
+		echo "About Page";
+	}
+}
